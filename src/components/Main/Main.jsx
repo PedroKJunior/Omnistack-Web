b7ee72fea2205f889919ee26be346af954adea15
Pedro Kretikouski Roque Júnior <pedro.junior@ringa.com.br>
2020-01-16 20:36:52 -0300

    Cria o componente de Main e adicionar os Cards

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from '../Card/Card'
+
+const Main = () => {
+    return (
+        <>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </>
+    )
+}
+export default Main
