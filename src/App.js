fc86e9713068348f2ab58a42c897b8aba1515529
Pedro Kretikouski Roque Júnior <pedro.junior@ringa.com.br>
2020-01-16 20:37:38 -0300

    Adiciona componentes a raiz do app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
+
 import './styles/global.sass'
 import './styles/app.sass'
 
+
+
 function App() {
   return (
-    <div id="App">
-      
+		<div id='app'>
+			<aside>
+        <Sidebar />
+			</aside>
+      <main>
+        <Main />
+      </main>
     </div>
   )
 }
