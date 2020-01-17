7f4ace28ffd7a3dbccd6f8ee06dc94e8f3b6d82c
Pedro Kretikouski Roque Junior <pedro.junior@ringa.com.br>
2020-01-17 15:54:41 -0300

    Insere propriedades tornando o componente dinâmico

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 import './../../styles/card.sass'
 
-const Card = () => {
+const Card = ({ img, name, techs, bio, github }) => {
     return (
         <div className="main">
             <ul>
                 <li className="dev-item">
                     <header>
-                        <img src="https://avatars2.githubusercontent.com/u/9387064?s=460&v=4" alt="Pedro Junior"/>
+                        <img src={img} alt={name}/>
                         <div className="user-info">
-                            <strong>Pedro K. R. Junior</strong>
-                            <span>React, Angular, Node.JS, Java</span>
+                            <strong> {name} </strong>
+                            <span> {techs} </span>
                         </div>
                     </header>
-                    <p>Sou uma pessoa que ama aprender e passar conhecimento, adoro desenvolvimento e designer, e apaixonado e curioso por algoritmos bio inspirados. =)</p>
-                    <a href="https://github.com/PedroKJunior">Acessar o perfil no Github</a>
+                    <p>{bio}</p>
+                    <a href={`https://github.com/${github}`}>Acessar o perfil no Github</a>
                 </li>
             </ul>
         </div>
