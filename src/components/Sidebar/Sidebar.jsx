26a1cf824c2358731ded79befa9c14284310cac0
Pedro Kretikouski Roque Júnior <pedro.junior@ringa.com.br>
2020-01-16 23:03:38 -0300

    Adiciona estado aos inputs

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,38 +1,79 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './../../styles/sidebar.sass'
 
 const Sidebar = () => {
+    
+    const [github_username, setGithubUserName] = useState('')
+    const [techs, setTechs] = useState('')
+    const [latitude, setLatitude] = useState('')
+    const [longitude, setLongitude] = useState('')
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const { latitude, longitude } = position.coords
+
+                setLatitude(latitude)
+                setLongitude(longitude)
+            }, 
+            err => {
+                console.log(err)
+            },{
+                timeout: 30000
+            }
+        )
+    },[])
+
     return (
         <>
             <strong>Cadastrar</strong>
             <form>
                 <div className='input-block'>
-                    <label htmlFor='github_username'>
-                        {' '}
-                        Usuário do Github{' '}
-                    </label>
                     <input
                         name='github_username'
                         id='github_username'
+                        value={github_username}
+                        onChange={ e => setGithubUserName(e.target.value) }
                         required
                     />
+                    <label htmlFor='github_username'>Usuário do Github</label>
                 </div>
 
                 <div className='input-block'>
+                    <input 
+                        name='techs' 
+                        id='techs' 
+                        value={techs}
+                        onChange={ e => setTechs(e.target.value) }
+                        required
+                    />
                     <label htmlFor='techs'> Tecnologias </label>
-                    <input name='techs' id='techs' required />
                 </div>
 
                 <div className='input-group'>
 
                     <div className='input-block'>
+                        <input 
+                            type='number' 
+                            name='latitude' 
+                            id='latitude' 
+                            required 
+                            value={latitude}
+                            onChange={ e => setLatitude(e.target.value) } 
+                        />
                         <label htmlFor='latitude'> Latitude </label>
-                        <input name='latitude' id='latitude' required />
                     </div>
 
                     <div className='input-block'>
+                        <input 
+                            type='number' 
+                            name='longitude'
+                            id='longitude' 
+                            required 
+                            value={longitude}
+                            onChange={ e => setLongitude(e.target.value) } 
+                        />
                         <label htmlFor='longitude'> Longitude </label>
-                        <input name='longitude' id='longitude' required />
                     </div>
 
                 </div>
